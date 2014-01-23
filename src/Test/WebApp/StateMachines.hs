0ad374fe5e5d42c0e6ebfdcfb508ee104defa58e
Matthias Fischmann <mf@zerobuzz.net>
2014-01-23 09:25:06 +0100

    rewrite module StateMachine

--- src/Test/WebApp/StateMachines.hs
+++ src/Test/WebApp/StateMachines.hs
@@ -5,14 +5,22 @@
 {-# LANGUAGE TupleSections                            #-}
 {-# LANGUAGE TypeFamilies                             #-}
 {-# LANGUAGE ViewPatterns                             #-}
+{-# LANGUAGE RecordWildCards                          #-}
+{-# LANGUAGE NamedFieldPuns                           #-}
 
 {- OPTIONS -fwarn-unused-imports #-}
 
+-- | FIXME: (1) update documentation.  (2) this is about state
+-- machines that mix both HTTP 'ScriptRq's / 'StoryItem's and a
+-- webdriver equivalent; this means we need to change types 'Script'
+-- and 'Story'.  for now, this module is only about HTTP.
 module Test.WebApp.StateMachines
 where
 
 import Control.Applicative
 import Control.Arrow
+import Control.Monad
+import Control.Monad.Trans
 import Data.Function
 import Data.List as List
 import Data.Map (Map)
@@ -33,8 +41,8 @@ import Test.WebDriver.Missing
 import Test.QuickCheck.Missing
 import Test.WebApp.WebDriver hiding (State (..), StateId, Machine (..), Trace (..),
                                      propTrace, propTrace', predecessors, successors, ancestors, descendants,
-                                     shortcuts, shortcuts', shortestTerminatingTrace)
-                               -- (all this will eventually be merged into here.)
+                                     shortcuts, shortcuts', shortestTerminatingTrace, arbitraryTrace)
+                               -- (all the above will eventually be merged into here.)
 
 
 
@@ -58,78 +66,43 @@ import Test.WebApp.WebDriver hiding (State (..), StateId, Machine (..), Trace (.
 -- for now, go with static transition maps, because otherwise it's
 -- just a turing machine an we can't do anything interesting to it but
 -- run it.
-data ProtoState payload content =
-    ProtoStateHTTP
-      { psStart        :: Bool
-      , psTerminal     :: Bool
-      , psEnterHTTP    :: payload -> Gen (ScriptRq content)
-      , psUpdateHTTP   :: forall payload' . payload' ~ payload => payload -> Either (NH.Response LBS) content -> payload'
-      , psTest         :: forall payload' . payload' ~ payload => payload' -> QC.Property
-      , psTransitions  :: [ProtoStateId]
-      }
-  | ProtoStateWD
-      { psStart        :: Bool
-      , psTerminal     :: Bool
-      , psEnterWD      :: payload -> Gen (WD (Maybe JS.Value))
-      , psUpdateWD     :: forall payload' . payload' ~ payload => payload -> Maybe JS.Value -> payload'
-      , psTest         :: forall payload' . payload' ~ payload => payload' -> QC.Property
-      , psTransitions  :: [ProtoStateId]
-      }
-
-
-instance Show (ProtoState payload content) where
-    show (ProtoStateHTTP start term enter update test trans) =
-        printf "(ProtoStateHTTP %s %s)" (show start) (show term)
-    show (ProtoStateWD start term enter update test trans) =
-        printf "(ProtoStateWD %s %s)" (show start) (show term)
-
-type ProtoStateId = Int
-
-
-protoStateHTTP :: (payload -> Gen (ScriptRq content))
-          -> (forall payload' . payload' ~ payload => payload -> Either (NH.Response LBS) content -> payload')
-          -> [ProtoStateId]
-          -> ProtoState payload content
-protoStateHTTP enter update trans = ProtoStateHTTP True True enter update (const $ mkprop True) trans
-
-
-protoStateWD :: (payload -> Gen (WD (Maybe JS.Value)))
-          -> (forall payload' . payload' ~ payload => payload -> Maybe JS.Value -> payload')
-          -> [ProtoStateId]
-          -> ProtoState payload content
-protoStateWD enter update trans = ProtoStateWD True True enter update (const $ mkprop True) trans
-
-
-data State payload content =
+data State content =
     StateHTTP
-      { sEnterHTTP    :: ScriptRq content
-      , sPayloadHTTP  :: payload
+      { stateStart        :: Bool
+      , stateTerminal     :: Bool
+      , stateTransitions  :: [StateId]
+      , stateHTTPRequest  :: Script content -> Gen (ScriptRq content)
+      , stateTest         :: Story content -> QC.Property
       }
-  | StateWD
-      { sEnterWD      :: WD (Maybe JS.Value)
-      , sUpdateWD     :: payload
-      }
+
+instance Show (State content) where
+    show (StateHTTP {..}) =
+        printf "(StateHTTP %s %s)" (show stateStart) (show stateTerminal) (show stateTransitions)
 
 
-instance (Show payload) => Show (State payload content) where
-    show (StateHTTP _ pl) =
-        printf "(StateHTTP %s)" (show pl)
-    show (StateWD _ pl) =
-        printf "(StateWD %s)" (show pl)
+-- | FIXME: this should be an application-specific enum type that is
+-- passed to 'State', 'Machine', ... as a parameter.
+type StateId = Int
 
 
--- | A machine is a mapping from 'ProtoStateId's to 'ProtoState's.  All other
+stateHTTP :: [StateId]
+          -> (Script content -> Gen (ScriptRq content))
+          -> State content
+stateHTTP trans step = StateHTTP True True trans step (const $ mkprop True)
+
+
+-- | A machine is a mapping from 'StateId's to 'State's.  All other
 -- information is contained in the states.
-data Machine payload content = Machine { fromMachine :: Map ProtoStateId (ProtoState payload content) }
+data Machine content = Machine { fromMachine :: Map StateId (State content) }
   deriving (Show)
 
 
--- | Collect all 'ProtoState's with 'ProtoStateId' of a machine, and create it.
--- Crashes if any 'ProtoStateId' occurs more than once.
-mkMachine :: [(ProtoStateId, ProtoState payload content)] -> Machine payload content
+-- | Collect all 'State's with 'StateId' of a machine, and create it.
+-- Crashes if any 'StateId' occurs more than once.
+mkMachine :: [(StateId, State content)] -> Machine content
 mkMachine states = if null dupeIds
                        then Machine $ Map.fromList states
-                       else error $ "mkMachine: duplicate ProtoStateIds: " ++ show dupeIds
+                       else error $ "mkMachine: duplicate StateIds: " ++ show dupeIds
   where
     ids = map fst states
     dupeIds = ids \\ nub ids
@@ -140,35 +113,26 @@ mkMachine states = if null dupeIds
 -- 'arbitrary' and 'shrink' on is one particular session at a time,
 -- with all state transition events chronological order.  We call this
 -- a 'Trace'.
-data Trace payload content = Trace (Machine payload content) [ProtoStateId]
+--
+-- FIXME: this should be called 'ProtoScript', and 'Script' should
+-- move to this module and allow for both http and webdriver items.
+data Trace content = Trace (Machine content) [StateId]
   deriving (Show)
 
--- | Like 'Trace', but contains the updated payload of each state
--- after execution.
-data Trace' payload content = Trace' (Machine payload content) [(ProtoStateId, State payload content)]
-  deriving (Show)
 
-
--- | Shrinking: This is currently just a call to 'shortcut'.  (Before
--- that, we should chop off the part of the script after the first
--- test failed, but this is done elsewhere.  FIXME: where?)
-shrinkTrace :: Trace payload content -> [Trace payload content]
-shrinkTrace = shortcuts
-
-
-arbitraryTrace :: forall payload content . Machine payload content -> Gen (Trace payload content)
+arbitraryTrace :: forall content . Machine content -> Gen (Trace content)
 arbitraryTrace machine = mkTrace <$> (mkStart >>= sized . f)
   where
-    mkStart :: Gen ProtoStateId
-    mkStart = case Map.keys . Map.filter psStart . fromMachine $ machine of
+    mkStart :: Gen StateId
+    mkStart = case Map.keys . Map.filter stateStart . fromMachine $ machine of
                 keys@(_:_) -> elements keys
                 [] -> error $ "arbitraryTrace: machine without start state: " ++ ppShow machine
 
-    mkTrace :: Maybe [ProtoStateId] -> Trace payload content
+    mkTrace :: Maybe [StateId] -> Trace content
     mkTrace (Just ids) = Trace machine ids
     mkTrace Nothing = error $ "arbitraryTrace: machine without terminator state: " ++ ppShow machine
 
-    f :: ProtoStateId -> Int -> Gen (Maybe [ProtoStateId])
+    f :: StateId -> Int -> Gen (Maybe [StateId])
     f last i = case (i, successors machine last) of
         (0, _)           -> terminate
         (_, [])          -> terminate
@@ -182,62 +146,29 @@ arbitraryTrace machine = mkTrace <$> (mkStart >>= sized . f)
         terminate = return $ shortestTerminatingTrace machine last
 
 
--- | (Could also be called 'runTrace'?)
-arbitraryTrace' :: forall payload content . Machine payload content -> Gen (Trace' payload content)
-arbitraryTrace' = error "wef"
+-- | Shrinking: This is currently just a call to 'shortcut'.  (Before
+-- that, we should chop off the part of the script after the first
+-- test failed, but this is done elsewhere.  FIXME: where?)
+shrinkTrace :: Trace content -> [Trace content]
+shrinkTrace = shortcuts
 
 
-
-{-
-
-propTrace :: WDSession -> (a -> Trace payload content) -> a -> QC.Property
-propTrace session fromTypedTrace typedTrace = propTrace' session (fromTypedTrace typedTrace)
-
-
--- | This property sais that a script satisfies all properties in all
--- intermediate states.
---
--- (This is sort of the replacement for the 'Story' type for http rest
--- api testing.  With selenium/webdriver, it is much less
--- straight-forward to represent the application state in a type
--- instantiating Show, Eq, ...)
-propTrace' :: WDSession -> Trace payload content -> QC.Property
-propTrace' session trace@(Trace machine []) = mkprop True  -- empty traces are boring
-propTrace' session trace@(Trace machine xs) =
-    morallyDubiousIOProperty $ do
-      -- debugM "" $ "propTrace: running " ++ ppShow trace
-      f xs
+-- | A 'Trace' defines a 'Script'.  (Starting from the empty script,
+-- follow the state ids and compute new requests from trace prefices.)
+-- Since state transitions do not depend on the trace prefix, it is
+-- useful to randomize it (and for that end wrap it into the 'Gen'
+-- monad.)
+arbitraryScriptFromTrace :: forall content . Trace content -> Gen (Script content)
+arbitraryScriptFromTrace (Trace machine stateIds) = foldM f (Script []) states
   where
-    f :: [ProtoStateId] -> IO QC.Property
-    f (idThis : idsRest) = do
-        debugM "" $ "propTrace': " ++ show idThis
+    f :: Script content -> State content -> Gen (Script content)
+    f script@(Script rqs) state@(StateHTTP {..}) = Script . (rqs ++) . (:[]) <$> stateHTTPRequest script
 
-        let stateThis :: ProtoState
-            Just stateThis = Map.lookup idThis $ fromMachine machine
+    states :: [State content]
+    states = catMaybes' $ map (`Map.lookup` (fromMachine machine)) stateIds
 
-        let runTest :: IO QC.Property
-            runTest = runWD session $ maybe successWD id $ sTest stateThis
-
-        case (sTerminal stateThis, idsRest) of
-          (True, [])
-              -> runTest
-          (False, [])
-              -> fail $ "propTrace': Trace ends in non-terminal state: " ++ ppShow (idThis, stateThis, trace)
-          (_, (idNext:_))
-              -> let transition :: WD ()
-                     transition = case lookup idNext $ sTransitions stateThis of
-                        Just t -> t
-                        Nothing -> error $ "propTrace': internal error: " ++ show (idNext, stateThis)
-
-                     step :: IO QC.Property
-                     step = runTest `finally` runWD session transition
-
-                     recurse :: IO QC.Property
-                     recurse = f idsRest
-
-                 in (.&&.) <$> step <*> recurse
-
--}
+    catMaybes' [] = []
+    catMaybes' (Just x:xs) = x : catMaybes' xs
 
 
 -- ** Graph algorithms
@@ -250,35 +181,35 @@ propTrace' session trace@(Trace machine xs) =
 --
 -- FIXME: for now, this only cuts out proper cycles, not detours like
 -- the example above.
-shortcuts :: Trace payload content -> [Trace payload content]
+shortcuts :: Trace content -> [Trace content]
 shortcuts (Trace machine xs) = map (Trace machine . reverse) . shortcuts' machine . reverse $ xs
 
 
 -- | Plumbing for 'shortcuts'.
-shortcuts' :: Machine payload content -> [ProtoStateId] -> [[ProtoStateId]]
+shortcuts' :: Machine content -> [StateId] -> [[StateId]]
 shortcuts' machine [] = []
 shortcuts' machine [only] = []
 shortcuts' machine (last:before@(second:before')) = (last:) <$> (direct before' ++ recursive)
   where
-    direct :: [ProtoStateId] -> [[ProtoStateId]]
+    direct :: [StateId] -> [[StateId]]
     direct [] = []
     direct candidate@(x:xs) = (if x == second then (candidate:) else id) $ direct xs
 
-    recursive :: [[ProtoStateId]]
+    recursive :: [[StateId]]
     recursive = shortcuts' machine before
 
 
 -- | Produce all possible direct successors (connected via a single
 -- transition) of a state.
-successors :: Machine payload content -> ProtoStateId -> [ProtoStateId]
-successors machine id = maybe [] psTransitions $ Map.lookup id $ fromMachine machine
+successors :: Machine content -> StateId -> [StateId]
+successors machine id = maybe [] stateTransitions $ Map.lookup id $ fromMachine machine
 
 -- | Produce all descentants of a state, ie. both direct successors
 -- and their successors, recursively.  Each descentant is annotated
 -- with the length of the path that it was reached on in the search
 -- (not necessarily the length of the shortest path to it!).  Output
 -- list is infinite iff there are cycles in the machine graph.
-descendants :: Machine payload content -> ProtoStateId -> [(Int, ProtoStateId)]
+descendants :: Machine content -> StateId -> [(Int, StateId)]
 descendants machine id = map (1,) direct ++ concat (map recursive direct)
   where
     direct = successors machine id
@@ -286,14 +217,14 @@ descendants machine id = map (1,) direct ++ concat (map recursive direct)
 
 -- | Like 'successors', but up the transition graph.  (In the current
 -- implementation, this is a bit more expensive than down.)
-predecessors :: Machine payload content -> ProtoStateId -> [ProtoStateId]
+predecessors :: Machine content -> StateId -> [StateId]
 predecessors machine id = map fst . filter hit . Map.toList . fromMachine $ machine
   where
-    hit (_, state) = id `elem` psTransitions state
+    hit (_, state) = id `elem` stateTransitions state
 
 -- | Like 'descendants' to 'successors', but calls 'predecessors'
 -- instead.
-ancestors :: Machine payload content -> ProtoStateId -> [(Int, ProtoStateId)]
+ancestors :: Machine content -> StateId -> [(Int, StateId)]
 ancestors machine id = map (1,) direct ++ concat (map recursive direct)
   where
     direct = predecessors machine id
@@ -302,21 +233,21 @@ ancestors machine id = map (1,) direct ++ concat (map recursive direct)
 -- | FIXME: 'descendants' and 'shortestTerminatingTrace' should both
 -- make use of more general graph algorithms.  Check king paper and
 -- ghc libs once more!
-shortestTerminatingTrace :: Machine payload content -> ProtoStateId -> Maybe [ProtoStateId]
+shortestTerminatingTrace :: Machine content -> StateId -> Maybe [StateId]
 shortestTerminatingTrace machine start = listToMaybe $ step [] start
   where
-    step :: [ProtoStateId] -> ProtoStateId -> [[ProtoStateId]]
+    step :: [StateId] -> StateId -> [[StateId]]
     step visited now
         | now `elem` visited  = []        -- (give up when hitting a cycle.)
         | terminal now        = [[now]]   -- (end when hitting a terminal state.)
-        | True                = let recurse :: [[ProtoStateId]]
+        | True                = let recurse :: [[StateId]]
                                     recurse = concatMap (step (now:visited)) (successors machine now)
                                 in sieve ((now:) <$> recurse)
 
-    terminal :: ProtoStateId -> Bool
-    terminal sid = case Map.lookup sid (fromMachine machine) of Just s -> psTerminal s
+    terminal :: StateId -> Bool
+    terminal sid = case Map.lookup sid (fromMachine machine) of Just s -> stateTerminal s
 
-    sieve :: [[ProtoStateId]] -> [[ProtoStateId]]
+    sieve :: [[StateId]] -> [[StateId]]
     sieve = map snd
           . sortBy (flip compare `on` fst)
           . map (\ ids -> (length ids, ids))
